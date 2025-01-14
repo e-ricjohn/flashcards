6178acb8c73c6a17af58397ee566ebd6041a6420
e-ricjohn <edj904@gmail.com>
2025-01-14 13:28:18 +0100

    14-1-25
    added preposition test sheet and tweaked the word_test function for verbs.

--- flashcards.py
+++ flashcards.py
@@ -1,41 +1,41 @@
 import csv
 import random
+import re
 import requests
 
 # URL of Google Sheets (CSV format)
 VOCAB_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRrMEqatYVAiFQcw6n_goN1O1xcko52D0sbj62FODLm4pWI3jeFz7WhkmXBbm5ES_gt3C2O9rE8ZorS/pub?gid=0&single=true&output=csv"
 WW_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQe7RCIpPJ-9dGuBfvoPHfTUelWdlhI4dziodqVbQRiEHrbc4p5tZWjOwjf2C4tGlZYcMHj6lgD3b78/pub?gid=256017748&single=true&output=csv"
-
+PREP_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRYmAhW1VtDeIldou941VHcXMI2wW6_7tCXuUCTfO7sOWAMtA7zRIxOHQ4qQ7qcZIxjDH6RhQF4QE_m/pub?output=csv"
 
 def main():
     # TODO: check / control user input
     # Prompt user for vocab or verb test.
-    prompt_Main = input("Vocab of werkworden? v/w ")
+    prompt_Main = input("Vocab / werkwoorden / preposities? v/w/p ")
 
     # Fetch data from selected google sheet.
     if prompt_Main == "v":
-        sheet = "v"
+        type = "v"
         data = fetch_data_from_sheet(VOCAB_SHEET_URL)
     elif prompt_Main == "w":
-        sheet = "w"
+        type = "w"
         data = fetch_data_from_sheet(WW_SHEET_URL)
+    elif prompt_Main == "p":
+        type = "p"
+        data = fetch_data_from_sheet(PREP_SHEET_URL)
 
     # TODO: check / control user input
     # Prompt user for chapter.
-    prompt_chapter = input("Welke hoofdstuk? ")
+    prompt_chapter = input("Welk hoofdstuk? # / alles: ")
     
     # Trim data to chapter. 
     data = chapter_select(data, prompt_chapter)
 
-    # Start test.
-    test_words(data, sheet)
+    # Test trimmed word list.
+    test_list(data, type)
+        
+    print("Alle woorden getest.")
 
-    # Pick random word.
-    # Test word.
-        # On first try,
-        # If wrong, save word in new dataset, and repeat until correct.
-        # If correct, remove from dataset.
-    # Repeat until data is empty.
 
 def fetch_data_from_sheet(url):
     try:
@@ -68,38 +68,96 @@ def fetch_data_from_sheet(url):
 
 
 def chapter_select(data, prompt_chapter):
-
-    chapter_data = [row for row in data if row["Hoofdstuk"] == prompt_chapter]
-
-    if chapter_data:
-        print("Flashcard Data: ")
-        for entry in chapter_data:
-            print(", ".join([f"{key}: {value}" for key, value in entry.items()]))
+    if prompt_chapter == "alles":
+        chapter_data = [row for row in data]
     else:
+        chapter_data = [row for row in data if row["Hoofdstuk"] == prompt_chapter]
+
+    if not chapter_data:
         print(f"No flaschards found for chapter {prompt_chapter}.")
+    else:
+        return chapter_data
 
-    return chapter_data
 
-
-def test_words(data, sheet):
+def get_random_word(data):
 
     if data:
-        random_row = random.choice(data)
-        user_answer = input(f"{random_row['Prompt']}")
-
-        if user_answer == random_row['Nederlands']:
-            return
-
+        index = random.randint(0, len(data) - 1)
+        return data[index], index
+    else:
+        print("No words found.")
 
 
-def test_ww():
-    # Fetch data
-    data = fetch_data_from_sheet(VOCAB_SHEET_URL)
+def test_list(list, type):
+    round = 1
+    repeat_list = [] 
 
-    print("Flashcard Data: ")
-    for entry in data:
-        print(", ".join([f"{key}: {value}" for key, value in entry.items()]))
+    # Check if list is empty after all rounds.
+    while list:
 
+        print(f"Ronde {round}.")
+
+        # Check if list is empty after one round.
+        while list:
+
+            # Pick random word.
+            word_data, index = get_random_word(list)
+
+            # Test word until success.
+            while not test_word(word_data, type):
+
+                # If failed test, check if word_data is already in repeat list.
+                if word_data not in repeat_list:
+                    # add data to repeat data.
+                    repeat_list.append(word_data)
+
+                pass
+
+            # pop row.
+            list.pop(index)
+        
+        # When list is empty, end round.
+        print("Ronde compleet.")
+        round += 1
+
+        # Copy repeat list to data.
+        list = repeat_list
+
+        # Clear repeat list.
+        repeat_list = []
+
+
+def test_word(word_data, type):
+    # assign prompt and answers with word data.
+    # handle vocab or preposition list type.
+    prompt = word_data["Prompt"]
+    if type == "v" or type == "p":
+        correct_answer1 = word_data["Nederlands"].lower()
+        correct_answer2 = re.sub(r"\(.*?\)", "", correct_answer1)
+        correct_answer2 = re.sub(r"\s+", " ", correct_answer2).strip()
+
+    # handle verb list type.
+    elif type == "w":
+        correct_answer1 = f"{word_data['Presens']} {word_data['Imperfectum']} {word_data['Perfectum']}"
+        correct_answer2 = re.sub(r"\(.*?\)", "", correct_answer1)
+        correct_answer2 = re.sub(r"\s+", " ", correct_answer2).strip()
+    
+    # ask user for answer 
+    answer = input(f"\n{prompt}\n\njouw antwoord:   ").lower().strip()
+
+    # print correct answer
+    if type == "w":
+        print(f"Juiste antwoord: {word_data['Presens']} | {word_data['Imperfectum']} | {word_data['Perfectum']}")
+    else:
+        print(f"Juiste antwoord: {correct_answer1}")
+
+    # check answer.
+    if answer == correct_answer1 or answer == correct_answer2:
+        print("\nJuist!\n")
+        return True
+    else:
+        print("\nProbeer het opnieuw...\n")
+        return False
 
 
 if __name__ == "__main__":
